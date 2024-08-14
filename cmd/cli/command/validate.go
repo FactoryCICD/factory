2b4efd0412962d5d916feec3f98ff8c43d3e9e9f
Josue Morales <molinajosue92@hotmail.com>
2024-08-14 00:08:04 -0400

    add parsing functions

--- cmd/cli/command/validate.go
+++ cmd/cli/command/validate.go
@@ -3,16 +3,11 @@ package command
 import (
 	"flag"
 	"fmt"
-	"io/fs"
-	"io/ioutil"
-	"log"
-	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/factoryci/factory"
 	"github.com/hashicorp/hcl/v2"
-	"github.com/spf13/afero"
 )
 
 // ValidateCommand is a Command implementation that validates factory files.
@@ -57,99 +52,9 @@ func (c *ValidateCommand) Run(rawArgs []string) int {
 // validate validates the given path by processing the directory, loading the files,
 // and returning any diagnostics encountered during the process.
 func (c *ValidateCommand) validate(path string) hcl.Diagnostics {
-	var diags hcl.Diagnostics
+	_, diags := factory.ParseDirectory(path, c.Recursive)
 
-	paths, dirDiags := c.processDir(path)
-	diags = diags.Extend(dirDiags)
-	log.Printf("[DEBUG] paths found: %s", paths)
-	fs := afero.NewOsFs()
-	parser := factory.NewParser(fs)
-
-	files, fileDiags := parser.LoadFiles(paths)
-	diags = diags.Extend(fileDiags)
-	fmt.Println(files)
 	return diags
-}
-
-// processDir processes the given directory path and returns a list of file paths and any diagnostics encountered.
-// If the path is a directory, it can be processed recursively if the `Recursive` flag is set.
-// If the path is a file, it will be treated as a single-element slice with the file info.
-// Only files with the ".hcl" extension will be included in the returned list of file paths.
-//
-// Parameters:
-//   - path: The directory path to process.
-//
-// Returns:
-//   - paths: A list of file paths found within the directory (including subdirectories if recursive).
-//   - diags: Any diagnostics encountered during the processing.
-func (c *ValidateCommand) processDir(path string) ([]string, hcl.Diagnostics) {
-	var diags hcl.Diagnostics
-	var paths []string
-
-	log.Printf("[DEBUG] terraform validate: processing directory %s", path)
-
-	info, err := os.Stat(path)
-	if err != nil {
-		diags = diags.Append(&hcl.Diagnostic{
-			Severity: hcl.DiagError,
-			Summary:  fmt.Sprintf("No file or directory at %s", path),
-			Detail:   err.Error(),
-		})
-		return paths, diags
-	}
-
-	var entries []fs.FileInfo
-	if info.IsDir() {
-		entries, err = ioutil.ReadDir(path)
-		if err != nil {
-			switch {
-			case os.IsNotExist(err):
-				diags = diags.Append(&hcl.Diagnostic{
-					Severity: hcl.DiagError,
-					Summary:  fmt.Sprintf("There is no directory at %s", path),
-					Detail:   err.Error(),
-				})
-			default:
-				diags = diags.Append(&hcl.Diagnostic{
-					Severity: hcl.DiagError,
-					Summary:  "Cannot read directory",
-					Detail:   err.Error(),
-				})
-			}
-			return paths, diags
-		}
-	} else {
-		// If the path is a file, create a single-element slice with the file info
-		// so that the rest of the code can treat it as paths from a directory.
-		entries = []fs.FileInfo{info}
-	}
-
-	for _, info := range entries {
-		name := info.Name()
-		subPath := filepath.Join(path, name)
-		if info.IsDir() {
-			// If the path is a directory, process
-			// it recursively if the flag is set.
-			if c.Recursive {
-				subPaths, subDiags := c.processDir(subPath)
-				paths = append(paths, subPaths...)
-				diags = diags.Extend(subDiags)
-			}
-
-			// If the directory is not recursive, skip it.
-			// This is the default behavior
-			continue
-		}
-
-		// The rest of this loop only applies to files
-		ext := filepath.Ext(name)
-		switch ext {
-		case ".hcl":
-			paths = append(paths, subPath)
-		}
-	}
-
-	return paths, diags
 }
 
 // Help implements cli.Command.
--- parser.go
+++ parser.go
@@ -2,6 +2,11 @@ package factory
 
 import (
 	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclparse"
@@ -70,4 +75,119 @@ func (p *Parser) LoadHCLFile(path string) (hcl.Body, hcl.Diagnostics) {
 	}
 
 	return file.Body, diags
+}
+
+// ParseFactoryDirectory parses the "factory/" directory and returns a slice of File pointers and any diagnostics encountered.
+//
+// Returns:
+//   - []*File: A slice of pointers to the parsed files.
+//   - hcl.Diagnostics: Any diagnostics encountered during parsing.
+func ParseFactoryDirectory() ([]*File, hcl.Diagnostics) {
+	return ParseDirectory("factory/", true)
+}
+
+// ParseDirectory parses the directory at the given path and returns a slice of File pointers and any diagnostics encountered.
+//
+// If the recursive flag is set to true, it will recursively process subdirectories as well.
+//
+// Parameters:
+//   - path: The directory path to process.
+//   - recursive: A flag indicating whether to process subdirectories recursively.
+//
+// Returns:
+//   - []*File: A slice of pointers to the parsed files.
+//   - hcl.Diagnostics: Any diagnostics encountered during parsing.
+func ParseDirectory(path string, recursive bool) ([]*File, hcl.Diagnostics) {
+	var diags hcl.Diagnostics
+
+	paths, dirDiags := processDir(path, recursive)
+	diags = diags.Extend(dirDiags)
+	fs := afero.NewOsFs()
+	parser := NewParser(fs)
+
+	files, fileDiags := parser.LoadFiles(paths)
+	diags = diags.Extend(fileDiags)
+
+	return files, diags
+}
+
+// processDir processes the given directory path and returns a list of file paths and any diagnostics encountered.
+// If the path is a directory, it can be processed recursively if the `Recursive` flag is set.
+// If the path is a file, it will be treated as a single-element slice with the file info.
+// Only files with the ".hcl" extension will be included in the returned list of file paths.
+//
+// Parameters:
+//   - path: The directory path to process.
+//
+// Returns:
+//   - paths: A list of file paths found within the directory (including subdirectories if recursive).
+//   - diags: Any diagnostics encountered during the processing.
+func processDir(path string, recursive bool) ([]string, hcl.Diagnostics) {
+	var diags hcl.Diagnostics
+	var paths []string
+
+	log.Printf("[DEBUG] terraform validate: processing directory %s", path)
+
+	info, err := os.Stat(path)
+	if err != nil {
+		diags = diags.Append(&hcl.Diagnostic{
+			Severity: hcl.DiagError,
+			Summary:  fmt.Sprintf("No file or directory at %s", path),
+			Detail:   err.Error(),
+		})
+		return paths, diags
+	}
+
+	var entries []fs.FileInfo
+	if info.IsDir() {
+		entries, err = ioutil.ReadDir(path)
+		if err != nil {
+			switch {
+			case os.IsNotExist(err):
+				diags = diags.Append(&hcl.Diagnostic{
+					Severity: hcl.DiagError,
+					Summary:  fmt.Sprintf("There is no directory at %s", path),
+					Detail:   err.Error(),
+				})
+			default:
+				diags = diags.Append(&hcl.Diagnostic{
+					Severity: hcl.DiagError,
+					Summary:  "Cannot read directory",
+					Detail:   err.Error(),
+				})
+			}
+			return paths, diags
+		}
+	} else {
+		// If the path is a file, create a single-element slice with the file info
+		// so that the rest of the code can treat it as paths from a directory.
+		entries = []fs.FileInfo{info}
+	}
+
+	for _, info := range entries {
+		name := info.Name()
+		subPath := filepath.Join(path, name)
+		if info.IsDir() {
+			// If the path is a directory, process
+			// it recursively if the flag is set.
+			if recursive {
+				subPaths, subDiags := processDir(subPath, recursive)
+				paths = append(paths, subPaths...)
+				diags = diags.Extend(subDiags)
+			}
+
+			// If the directory is not recursive, skip it.
+			// This is the default behavior
+			continue
+		}
+
+		// The rest of this loop only applies to files
+		ext := filepath.Ext(name)
+		switch ext {
+		case ".hcl":
+			paths = append(paths, subPath)
+		}
+	}
+
+	return paths, diags
 }
